d6b09d879c894b5f4a1c9f78f6e0481f6045e375
mkirsch2 <95597392+mkirsch2@users.noreply.github.com>
2022-04-10 17:22:51 -0600

    images and R file

---
+++ MechaCarChallenge.R
@@ -0,0 +1,39 @@
+# Deliverable 1
+
+library(dplyr)
+
+mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg_df)
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+             ground_clearance + AWD, data = mechacar_mpg_df))
+
+# Deliverable 2
+
+suspension_coil <- read.csv(file="Suspension_Coil.csv", check.names=F,stringsAsFactors = F)
+
+total_summary <- suspension_coil %>%
+  summarize(Mean = mean(PSI),
+            Median = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI))
+
+lot_summary = suspension_coil %>%
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI),
+            Median = median(PSI),
+            Variance = var(PSI),
+            SD = sd(PSI),
+            .groups = "keep")
+
+# Deliverable 3
+
+t.test(suspension_coil$PSI,mu=1500)
+
+lot1 <- subset(suspension_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
